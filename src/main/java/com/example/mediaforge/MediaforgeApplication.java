7bdd538538a10005494ee4fc3ed9808e2a90dab7
Sonali Acharya <acharyasonali2006@gmail.com>
2026-09-23 13:48:25 +0530

    Initial MediaForge Setup

---
+++ src/main/java/com/example/mediaforge/MediaforgeApplication.java
@@ -0,0 +1,13 @@
+package com.example.mediaforge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediaforgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediaforgeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mediaforge/MediaforgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mediaforge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediaforgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
